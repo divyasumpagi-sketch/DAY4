62f7892616a95ed27be02451f5198ac7ebfc5046
Divya <divyasumpagi@gmail.com>
2026-02-07 19:54:25 +0530

    Add files via upload
    This Python program demonstrates the use of both for and while loops. The for loop prints numbers from 1 to 10, and the while loop prints even numbers from 2 to 10. It helps in understanding loop structures and number sequences in Python.

---
+++ whileloop.py
@@ -0,0 +1,12 @@
+# Program using for and while loops
+
+print("Print numbers from 1 to 10")
+for i in range(1, 11):
+    print(i, end=" ")
+
+print("\n")
+print("Print even numbers from 2 to 10")
+j = 2
+while j <= 10:
+    print(j, end=" ")
+    j += 2
